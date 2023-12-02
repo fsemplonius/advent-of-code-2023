7e9301626b11f83007cc7b24b8fcd3947bfd02b4
Folkert Semplonius <folkert@semplonius.info>
2023-12-02 12:13:56 +0100

    Added day 2 - mod

--- src/AOC_2302-2.php
+++ src/AOC_2302-2.php
@@ -7,10 +7,8 @@ $tot = 0;
 while ($line=next($input)) {
   $min = array ('red' => 0, 'green'=> 0, 'blue' => 0);
   list ($game, $sets) = explode(': ', $line);
-  $cubes = explode('; ', $sets);
-  foreach ($cubes as $set) {
-    $cubes = explode(', ', $set);
-    foreach ($cubes as $cube) {
+  foreach (explode('; ', $sets) as $set) {
+    foreach (explode(', ', $set) as $cube) {
       list ($num, $color) = explode(' ' , $cube);
       $min[$color] = max($min[$color], $num);
     }
